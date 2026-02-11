3ae917ab441404dcf75414971cfa238d53c83098
LangsungKlik.id <yazidbusthomy27@gmail.com>
2026-02-12 04:36:14 +0700

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Fix for some libraries using HashRouter in Next.js
-  output: 'standalone',
-};
-
-export default nextConfig;
