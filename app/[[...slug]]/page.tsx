630aa0382f0c3b9fe15e50406dda812f965e9e5a
LangsungKlik.id <yazidbusthomy27@gmail.com>
2026-02-12 04:32:21 +0700

    Delete app directory

--- app/[[...slug]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-// Import App.tsx dynamically with SSR disabled
-// This allows HashRouter and localStorage to work without server hydration errors
-const ClientApp = dynamic(() => import('../../App'), { ssr: false });
-
-export default function Page() {
-  return <ClientApp />;
-}
